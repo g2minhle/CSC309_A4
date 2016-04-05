c088f77d54e3e6aca9702461c98f1a5d72c7fd49
g2minhle <lehoangminh@live.com>
2016-04-05 13:30:40 -0400

    uthentication.js: Change to localhost, not personal ip address

--- part2/fitness_connection/config/authentication.js
+++ part2/fitness_connection/config/authentication.js
@@ -4,7 +4,7 @@ module.exports = {
     facebookConfig: {
         clientID: "603175176512414",
         clientSecret: "dc1d46f53f4ad7b7779e4e5703d75e12",
-        callbackURL: "http://192.168.12.113:3000/auth/facebook/callback"
+        callbackURL: "http://localhost:3000/auth/facebook/callback"
     },
 
     localConfig: {
