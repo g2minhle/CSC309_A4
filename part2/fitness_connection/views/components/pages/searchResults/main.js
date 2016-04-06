594e2dc5a65d1cdeabb9bc77c46136799690aacd
Ubuntu User <adamgj.wong@gmail.com>
2016-04-05 21:58:46 -0400

    bootstrap added to search results

--- part2/fitness_connection/views/components/pages/searchResults/main.js
+++ part2/fitness_connection/views/components/pages/searchResults/main.js
@@ -45,8 +45,128 @@ Container = React.createClass({
  
     render: function() { 
 
+        var trainerHTML = this.state.recommendations.map(function(trainer) {
+            return (
+                    <div className="trainer_bio">
+                        <div className="row">
+                            <div className="profile_left col-md-2">
+                                <img className="img-circle profile_img" src="/statics/imgs/default_profile.jpg"/>
+                            </div>
+                            <div className="profile_center col-md-7">
+                                <ul>
+                                    <li><span className="font-bold">Name: </span> {trainer.firstName} {trainer.lastName}</li>
+                                    <li><span className="font-bold">Sport: </span> {trainer.sports}</li>
+                                    <li><span className="font-bold">Rating: </span>
+                                        <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                        <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                        <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                        <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                        <span className="glyphicon glyphicon-star-empty" aria-hidden="true"></span> (2 Reviews)
+                                    </li>
+
+                                    <li><span className="font-italic">
+                                        {trainer.experience}
+                                        </span>
+                                    </li>
+
+                                    <li><span className="font-bold">Location: </span> {trainer.location}
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="profile_right col-md-3">
+                                <h2>${trainer.price}</h2>
+                                <a href="./profilePage.html" className="btn btn-lg btn-primary btn-block">View Profile</a>
+                                <a href="./book.html" className="btn btn-lg btn-danger btn-block">Book Now!</a>
+                            </div>
+                        </div>
+                    </div>
+                    );
+        });
+
         return (
-    
+                    <div className="container">
+                    <h1>Recommended Personal Trainers Near You</h1>
+                    {trainerHTML};
+                    </div>
+                );
+    }
+}),/*
+
+
+        <div className="trainer_bio">
+            <div className="row">
+                <div className="profile_left col-md-2">
+                    <img className="img-circle profile_img" src="views/statics/imgs/default_profile.jpg">
+                </div>
+                <div className="profile_center col-md-7">
+                    <ul>
+                        <li><span className="font-bold">Name: </span> Ron Runner</li>
+                        <li><span className="font-bold">Sport: </span> Cross-Country</li>
+                        <!-- TODO: Make this cleaner? -->
+                        <li><span className="font-bold">Rating: </span> 
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            (4 Reviews)
+                        </li>
+
+                        <li><span className="font-italic">
+                            "GOTTA RUN FAST.  GO GO GO"</span>
+                        </li>
+
+                        <li><span className="font-bold">Location: </span> Longest Road, San Shoes (&lt; 25 km)
+                        </li>
+                    </ul>
+                </div>
+                <div className="profile_right col-md-3"> 
+                    <h1>$15</h2>
+                    <a href="./profilePage.html" className="btn btn-lg btn-primary btn-block">View Profile</a>
+                    <a href="./book.html" className="btn btn-lg btn-danger btn-block">Book Now!</a>
+                </div>
+            </div>
+        </div>
+
+        <div className="trainer_bio">
+            <div className="row">
+                <div className="profile_left col-md-2">
+                    <img className="img-circle profile_img" src="views/statics/imgs/default_profile.jpg">
+                </div>
+                <div className="profile_center col-md-7">
+                    <ul>
+                        <li><span className="font-bold">Name: </span> Mathew Basketcase</li>
+                        <li><span className="font-bold">Sport: </span> Basketball</li>
+                        <!-- TODO: Make this cleaner? -->
+                        <li><span className="font-bold">Rating: </span> 
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+                            <span className="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+                            (4 Reviews)
+                        </li>
+
+                        <li><span className="font-italic">
+                            "Captain of the highschool basketball team.  Twice."</span>
+                        </li>
+
+                        <li><span className="font-bold">Location: </span> Los Balls, Basketland (&lt; 10 km)
+                        </li>
+                    </ul>
+                </div>
+                <div className="profile_right col-md-3"> 
+                    <h1>$125</h2>
+                    <a href="./profilePage.html" className="btn btn-lg btn-primary btn-block">View Profile</a>
+                    <a href="./book.html" className="btn btn-lg btn-danger btn-block">Book Now!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
         <div className="container">
             <div className="row">
                 <hr/>
@@ -93,7 +213,7 @@ Container = React.createClass({
         </div>
         
     ); }
-}),
+}),*/
 
 Index = React.createClass({
     render: function() { return (
