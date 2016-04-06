c2a6a788b014f5f63942e1b416eb9b0836148bdb
cuziluvtosmile <nidhi.kantak@mail.utoronto.ca>
2016-04-06 13:53:39 -0400

    Update database.js
    code to wipe off database and close connection

--- part2/fitness_connection/models/database.js
+++ part2/fitness_connection/models/database.js
@@ -6,3 +6,25 @@ module.exports = {
         database.connect(config.url);
     }
 };
+// wiping database
+//drop database so all data is wiped off
+
+mongoose.connection.on('open', function(){
+	module.exports = {
+		init: function(){
+			mongoose.connection.db.dropDatabase(function(err) {
+				console.log(err)
+			})
+    	}
+	}
+});
+//closing connection
+mongoose.connection.on('open', function(){
+	module.exports = {
+		init: function(){
+			mongoose.connection.close(function(err) {
+				console.log(err)
+			})
+    	}
+	}
+});
