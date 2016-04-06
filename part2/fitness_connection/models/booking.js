c221a41f53e9c54e0ae4549734d3d61a714744a5
cuziluvtosmile <nidhi.kantak@mail.utoronto.ca>
2016-04-06 17:51:38 -0400

    Update booking.js

--- part2/fitness_connection/models/booking.js
+++ part2/fitness_connection/models/booking.js
@@ -12,8 +12,25 @@ var booking = mongoose.Schema({
     },
     dateCreated: { type: Date, default: Date.now }
 });
+// separate schema for messages and this is accessed by admin to pass
+//message from one user to another
+var messages = mongoose.Schema({
+    from: {
+    	type: mongoose.Schema.ObjectId,
+    	ref: 'Sender'
+    },
+    to:{
+    	type: mongoose.Schema.ObjectId,
+    	ref: 'Receiver'
+    };
+    message:{
+    	type: mongoose.Schema.ObjectId,
+    	ref: 'Message'
+    }
+    timeStamp: { type: Date, default: Date.now }	
+});
 
-/*
- * 
- */
+
 module.exports = mongoose.model('Booking', booking);
+module.exports = mongoose.model('Messages', messages);
+
