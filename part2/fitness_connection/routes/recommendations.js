36304f6bc4893691238ac5ae126be64fbbb778e9
g2minhle <lehoangminh@live.com>
2016-04-06 11:55:39 -0400

    Clean up recommendation and search result

--- part2/fitness_connection/routes/recommendations.js
+++ part2/fitness_connection/routes/recommendations.js
@@ -1,54 +1,24 @@
-var express = require('express');
-var router = express.Router();
+var
+    express = require('express'),
+    authCore = require('../modules/authentication/authCore'),
+    Trainer = require('../models/user');
 
-var mongoose = require('mongoose');
-var url = 'mongodb://localhost:27017/fitness_connection';
-
-var Trainer = require('../models/user');
+var
+    router = express.Router();
 
 /* GET search results */
 router.get('/', function(req, res, next) {
-
-  res.render('page', { pageName: 'recommendations' });
+    res.render('page', { pageName: 'recommendations' });
 });
 
 router.get('/getRecommendations', function(req, res, next) {
-
-    // Insert dummy trainer data
-    var insertDummy = function() {
-
-      var names = ["Adam", "Bob", "Candice", "Dennis", "Early"];
-
-      for (var i = 0; i < 5; i++) {
-
-        var newTrainer = Trainer({
-        userid: i,
-        firstName: names[i],
-        price: 50.00,
-        rating: 2
-        });
-
-        newTrainer.save(function(err) {
-          if (err) console.log(err);
-
-          console.log("Saved dummy trainer");
-        });      
-      };
-    }
-    
     // Do smart search algo based on user location and trainer location for recommended trainers
-    var findTrainers = function() {
-       Trainer.find({"location" : req.body.location }, function (err, docs) {
+    Trainer.find({ "location": req.body.location }, function(err, docs) {
 
         // Get top 3 hits
         var recommendations = docs.slice(0, 3);
         res.send(recommendations);
-      });
-    }
-    
-    insertDummy();
-    findTrainers();
-
-});    
+    });
+});
 
 module.exports = router;
--- part2/fitness_connection/routes/searchResults.js
+++ part2/fitness_connection/routes/searchResults.js
@@ -1,11 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var 
+    express = require('express'),
+    Trainer = require('../models/user');
 
-var mongoose = require('mongoose');
-var url = 'mongodb://localhost:27017/fitness_connection';
-
-var Trainer = require('../models/user');
-
+var 
+    router = express.Router();
+    
 /* GET search results */
 router.get('/', function(req, res, next) {
 
@@ -14,46 +13,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:keyword', function(req, res, next) {
-
-    console.log("doing search");
-    // Insert dummy trainer data
-    var insertDummy = function() {
-
-      var names = ["Adam", "Bob", "Candice", "Dennis", "Early"];
-
-      for (var i = 0; i < 5; i++) {
-
-        var newTrainer = Trainer({
-        userid: i,
-        firstName: names[i],
-        price: 50.00,
-        rating: 5
-        });
-
-        newTrainer.save(function(err) {
-          if (err) console.log(err);
-
-          console.log("Saved dummy trainer");
-        });      
-      };
-    }
-    
-    // /search?keyword=<keyword>
+    //search?keyword=<keyword>
     console.log("Performing search for keyword: " + req.params.keyword);
-
-
-    // Do smart search algo based on user location and trainer location for recommended trainers
-    var findTrainers = function() {
-       Trainer.find({ $or: [ {"location" : req.params.keyword }, {"firstName" : req.params.keyword },
-                             {"lastName" : req.params.keyword }]}, function (err, docs) {
-
-        res.send(docs);
-      });
-    }
-    
-    insertDummy();
-    findTrainers();
-
+    Trainer.find({ $or: [ 
+            {"location" : req.params.keyword }, 
+            {"firstName" : req.params.keyword },
+            {"lastName" : req.params.keyword }]}, 
+            function (err, docs) {
+                res.send(docs);
+            }
+    );
 });    
 
 module.exports = router;
