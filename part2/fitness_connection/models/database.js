6f2ef9ac43e8729133640f497a77da8922bc02d9
cuziluvtosmile <nidhi.kantak@mail.utoronto.ca>
2016-04-06 17:20:59 -0400

    Update database.js

--- part2/fitness_connection/models/database.js
+++ part2/fitness_connection/models/database.js
@@ -28,3 +28,28 @@ database.connection.on('open', function(){
     	}
 	}
 });
+// exporting data from one database to another 
+var query;
+var collection;
+var firstDatabase;
+var otherDatabase
+
+init:function(query,collection,firstDatabase,otherDatabase){
+	firstDatabase.collection.find({query}).forEach(function(d)
+		{ firstDatabase.getSiblingDB(otherDatabase)
+			[collection].insert(d)});
+	firstDatabase.collection.find({query}).forEach(function(d){
+	});
+	firstDatabase.collection.remove({query});
+}
+
+//importing data from one database to another 
+
+init:function(query,collection,firstDatabase,otherDatabase){
+	otherDatabase.collection.find({query}).forEach(function(d)
+		{ otherDatabase.getSiblingDB(firstDatabase)
+			[collection].insert(d)});
+	otherDatabase.collection.find({query}).forEach(function(d){
+	});
+	otherDatabase.collection.remove({query});
+}
