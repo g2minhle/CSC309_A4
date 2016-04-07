f854a51a81774e1f0e7a87bcd58d15bb76584db9
Ubuntu User <adamgj.wong@gmail.com>
2016-04-06 23:34:20 -0400

    fixed merge

--- part2/fitness_connection/routes/admin.js
+++ part2/fitness_connection/routes/admin.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var current_db;
 var mongoose = require('mongoose');
 var url = 'mongodb://localhost:27017/fitness_connection';
 
@@ -42,39 +42,7 @@ router.get('/updateInbox', function(req, res, next) {
       });
     }
     findMessages();
-});    
-var express = require('express');
-var router = express.Router();
-var current_db;
-var mongoose = require('mongoose');
-var url = 'mongodb://localhost:27017/fitness_connection';
-
-var Trainer = require('../models/user');
-
-/* GET search results */
-router.get('/', function(req, res, next) {
-  
-  res.render('page', { pageName: 'admin' });
-});
-
-router.get('/getAllUsers', function(req, res, next) {
-
-    console.log("doing search");
-
-    // /search?keyword=<keyword>
-    console.log("Performing search for all users...");
-
-
-    // Find all users
-    var findTrainers = function() {
-       Trainer.find({}, function (err, docs) {
-
-        res.send(docs);
-      });
-    }
-    
-    findTrainers();
-});
+});  
 
 router.post('/setPassword', function(req, res, next) {
 
