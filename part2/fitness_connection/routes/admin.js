e34a6c957937aad07d329ff48502b6c31783a49b
cuziluvtosmile <nidhi.kantak@mail.utoronto.ca>
2016-04-06 21:59:05 -0400

    Update admin.js

--- part2/fitness_connection/routes/admin.js
+++ part2/fitness_connection/routes/admin.js
@@ -43,5 +43,53 @@ router.get('/updateInbox', function(req, res, next) {
     }
     findMessages();
 });    
+var express = require('express');
+var router = express.Router();
+var current_db;
+var mongoose = require('mongoose');
+var url = 'mongodb://localhost:27017/fitness_connection';
 
+var Trainer = require('../models/user');
+
+/* GET search results */
+router.get('/', function(req, res, next) {
+  
+  res.render('page', { pageName: 'admin' });
+});
+
+router.get('/getAllUsers', function(req, res, next) {
+
+    console.log("doing search");
+
+    // /search?keyword=<keyword>
+    console.log("Performing search for all users...");
+
+
+    // Find all users
+    var findTrainers = function() {
+       Trainer.find({}, function (err, docs) {
+
+        res.send(docs);
+      });
+    }
+    
+    findTrainers();
+});
+
+router.get('/wipe', function(req, res, next) {
+
+    console.log("wipe");
+
+    // /search?keyword=<keyword>
+    console.log("Wiping data");
+
+
+    // Find all users
+    var wipeData = function() {
+          wipe(current_db);
+      };
+    wipeData();
+});
+
+module.exports = router;
 module.exports = router;
