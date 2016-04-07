ca781ac211ea5684d98db8f05cd374a8f8b9e216
g2minhle <lehoangminh@live.com>
2016-04-06 22:20:39 -0400

    update model forbooking feature

--- part2/fitness_connection/models/booking.js
+++ part2/fitness_connection/models/booking.js
@@ -2,35 +2,11 @@ var mongoose = require('mongoose');
 
 // define the schema for our user model
 var booking = mongoose.Schema({
-    trainer: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User'
-    },
-    trainee: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User'
-    },
+    trainerId: String,
+    traineeId: String,
+    time: String,
     dateCreated: { type: Date, default: Date.now }
 });
-// separate schema for messages and this is accessed by admin to pass
-//message from one user to another
-var messages = mongoose.Schema({
-    from: {
-    	type: mongoose.Schema.ObjectId,
-    	ref: 'Sender'
-    },
-    to:{
-    	type: mongoose.Schema.ObjectId,
-    	ref: 'Receiver'
-    };
-    message:{
-    	type: mongoose.Schema.ObjectId,
-    	ref: 'Message'
-    }
-    timeStamp: { type: Date, default: Date.now }	
-});
-
 
 module.exports = mongoose.model('Booking', booking);
-module.exports = mongoose.model('Messages', messages);
 
